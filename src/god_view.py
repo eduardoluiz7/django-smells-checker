410bc1eb9a1c1b8eeff5736f43035e5d9ad3a1e0
Eduardo Luiz <el6@cin.ufpe.br>
2024-03-10 22:59:08 -0300

    cleaning prints

--- src/god_view.py
+++ src/god_view.py
@@ -30,39 +30,3 @@ def detect_god_view_smell(code):
     tree = ast.parse(code)
     visitor = ComplexityVisitor(code)
     visitor.visit(tree)
-
-# Exemplo de uso
-code_example = """
-def complex_user_view(request):
-    if request.method == 'GET':
-        users = User.objects.all()
-        user_data = []
-        for user in users:
-            profile = UserProfile.objects.get(user=user)
-            user_data.append({
-                'username': user.username,
-                'email': user.email,
-                'profile_pic': profile.profile_picture_url,
-                'bio': profile.bio
-            })
-        return HttpResponse(str(user_data))
-
-    elif request.method == 'POST':
-        # Simulando processamento complexo de dados de entrada
-        data = request.POST
-        new_user = User(username=data['username'], email=data['email'])
-        new_user.save()
-        new_profile = UserProfile(user=new_user, bio=data['bio'], profile_picture_url=data['profile_pic'])
-        new_profile.save()
-
-        return HttpResponse("User created successfully!")
-
-    elif request.method == 'PUT':
-        # Simulando outra operação complexa
-        return HttpResponse("User updated successfully!")
-
-    else:
-        return HttpResponse("Unsupported HTTP method", status=405)
-"""
-
-detect_god_view_smell(code_example)
